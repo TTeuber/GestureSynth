62d199b30840baa728cfeed47b2c4e242f2b52b3
tylerteuber <tyler.teuber@gmail.com>
2026-03-07 19:21:52 -0800

    Refactored modulation source colors by introducing descriptive constants for improved readability and maintainability.

--- source/Theme.h
+++ source/Theme.h
@@ -12,13 +12,18 @@
 #define TEXT_INACTIVE_COLOR juce::Colour::fromRGB (110, 110, 110)
 #define MOD_COLOR juce::Colours::cyan
 
+#define ENV_COLOR juce::Colours::lightgreen
+#define LFO_COLOR juce::Colours::gold
+#define CONTROLLER_COLOR juce::Colours::lightcoral // aftertouch, expression, mod wheel
+#define KEY_VEL_COLOR juce::Colours::deepskyblue // velocity, keyboard
+
 inline auto getModColor (const juce::String& sourceID) -> juce::Colour
 {
     if (sourceID.startsWith ("lfo"))
-        return juce::Colours::yellow;
+        return LFO_COLOR;
     if (sourceID.startsWith ("env"))
-        return juce::Colours::green;
+        return ENV_COLOR;
     if (sourceID == "modWheel" || sourceID == "aftertouch" || sourceID == "expression")
-        return juce::Colours::magenta;
-    return juce::Colours::cyan; // keyboard, velocity, fallback
+        return CONTROLLER_COLOR;
+    return KEY_VEL_COLOR; // keyboard, velocity, fallback
 }
